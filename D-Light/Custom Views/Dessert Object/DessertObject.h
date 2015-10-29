9edba13ef61a4f518ed8ec5aba2af6d4ab411384
Krystel Chaccour <k.chaccour@syncworx.com>
2015-10-29 15:47:28 +0200

    Dessert Object: a RLMObject class with the used properties

--- D-Light/Custom Views/Dessert Object/DessertObject.h
+++ D-Light/Custom Views/Dessert Object/DessertObject.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-@interface DessertObject : NSObject
+@interface DessertObject : RLMObject
+
+@property (nonatomic, strong) NSString *dessert_id;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *tags;
+@property (nonatomic, strong) NSString *desc;
+@property (nonatomic, strong) NSString *prep_time;
+@property (nonatomic, strong) NSString *cook_time;
+@property (nonatomic, strong) NSString *total_time;
+@property (nonatomic, strong) NSString *calories;
+@property (nonatomic, strong) NSString *ingredients;
+@property (nonatomic, strong) NSString *recipe;
+@property (nonatomic, strong) NSString *image;
+@property (nonatomic, strong) NSString *source;
 
 @end
